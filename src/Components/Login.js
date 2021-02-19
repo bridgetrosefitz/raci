63eac009d47ce04d3e8f48911f033ae85e9eaba9
bridgetrosefitz <bridgetrosefitz@gmail.com>
2021-02-19 15:10:05 +0100

    Sets up handleButtonClick on Login

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import { Input } from 'semantic-ui-react';
-import { Link } from 'semantic-ui-react';
+import { Button } from 'semantic-ui-react';
 
 export default class Login extends React.Component {
   constructor() {
     super()
 
-    // this.state = {
+    this.state = {
+      email: null,
+      password: null,
+    }
+  }
 
-    // }
+  handleButtonClick = () => {
+    fetch('http://localhost:3001/api/v1/projects')
+    .then(res => res.json())
+    .then(data => console.log(data))
   }
 
   render(){
@@ -18,6 +25,8 @@ export default class Login extends React.Component {
         <Input placeholder='Your email' />
         <br/>
         <Input placeholder='Password' />
+        <br/>
+        <Button onClick={this.handleButtonClick}>Log in</Button>
         <br/>
         <a>Forgot password?</a> or <a>Sign up</a>
       </div>
