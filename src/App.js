f8d199759f17c1062323a020c161b0c3f2af9808
bridgetrosefitz <bridgetrosefitz@gmail.com>
2021-02-19 22:07:18 +0100

    In Login, replaces Input with Form; sets up controlled form for login; sets up token saving in local storage, adds Profile and Signup components

--- src/App.js
+++ src/App.js
@@ -2,26 +2,39 @@ import './App.css';
 import React from 'react';
 import RACITable from './Components/RACITable';
 import Login from './Components/Login';
+import Profile from './Components/Profile';
 import 'semantic-ui-css/semantic.min.css';
 import { Container } from 'semantic-ui-css/semantic.min.css';
 
 class App extends React.Component {
 
   state = {
-    page: 'login'
+    page: ''
+  }
+
+  redirect = (page) => {
+    this.setState({ page: page })
+  }
+
+  componentDidMount() {
+    if (localStorage.token) {
+      this.redirect('profile')
+    }
   }
 
   render () {
     // <Container>{
     switch(this.state.page) {
       case ('login'):
-        return <Login />
+        return <Login redirect={this.redirect}/>
       case ('RACITable'):
         return <RACITable/>
+      case ('profile'):
+        return <Profile />
       default:
         return <Login/>
     }
-  }
+  } 
   // </Container>
 }
 
--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,51 +1,67 @@
 import React from 'react';
-import { Input } from 'semantic-ui-react';
-import { Button } from 'semantic-ui-react';
+import { Input, Button, Form } from 'semantic-ui-react';
 
 export default class Login extends React.Component {
   constructor() {
     super()
 
     this.state = {
-      email: `kevin@test.com`,
-      password: `beef`,
-      user_id: null
+      email: '',
+      password: '',
     }
   }
 
-  // controlledForm = () => {
-
-  // }
-
-  handleButtonClick = () => {
-    const userEmail = this.state.email
-    const userPassword = this.state.password
-    fetch(`http://localhost:3001/api/v1/login`, {
+  handleSubmit = (e) => {
+    e.preventDefault()
+    fetch(`http://localhost:3002/api/v1/login`, {
       method: 'POST',
       headers: {
         'Content-Type' : 'application/json',
         'Accept' : 'application/json'
       },
-      body: JSON.stringify({
-        "email" : userEmail,
-        "password" : userPassword
-      })
+      body: JSON.stringify(this.state)
     })
     .then(res => res.json())
     .then(data => {
-      debugger
-      })
+      if(data.token) {
+      localStorage.token = data.token
+      this.props.redirect('profile')
+      }
+    })
   }
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
 
   render(){
     return(
       <div>
         <h2>Login</h2>
-        <Input placeholder='Your email' type='text'/>
-        <br/>
-        <Input placeholder='Password' type='password'/>
-        <br/>
-        <Button onClick={this.handleButtonClick}>Log in</Button>
+        <Form>
+          <Input 
+            placeholder='Your email' 
+            type='text' 
+            name='email'
+            value={this.state.email}
+            onChange={this.handleChange}/>
+          <br/>
+          <Input 
+            placeholder='Password' 
+            type='password' 
+            name='password'
+            value={this.state.password}
+            onChange={this.handleChange}/>
+          <br/>
+          <Button 
+            onClick={this.handleSubmit}
+            type='submit'>
+            Log in
+          </Button>
+        </Form>
         <br/>
         <a>Forgot password?</a> or <a>Sign up</a>
       </div>
