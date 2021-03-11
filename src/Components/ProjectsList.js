bdc7f7449397bfb9097116f4378565cbdad35041
bridgetrosefitz <bridgetrosefitz@gmail.com>
2021-03-11 23:58:02 +0100

    Work in progress: Setting up fetch request for projects index page

--- src/Components/ProjectsList.js
+++ src/Components/ProjectsList.js
@@ -2,7 +2,22 @@ import React from 'react';
 
 export default class ProjectsList extends React.Component {
 
-  
+  constructor() {
+    super();
+
+    this.state = {
+      projects: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3001/api/v1/projects')
+      .then(res => res.json())
+      .then(data => this.setState({
+        projects: data.data
+      }))
+  }
+
   render() {
     return (
       <div>{
