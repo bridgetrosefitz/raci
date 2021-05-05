a2550ab4f2ac38b256ec1ed0302ae2b7ef9720e1
bridgetrosefitz <bridgetrosefitz@gmail.com>
2021-05-05 12:28:48 -0400

    Controlled forms in signup page

--- src/Components/SignUp.js
+++ src/Components/SignUp.js
@@ -16,6 +16,17 @@ export default class SignUp extends React.Component {
     }
   }
 
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    fetch('http://localhost:3001/users')
+  }
+
   render () {
     return (
       <div>
@@ -25,30 +36,41 @@ export default class SignUp extends React.Component {
           placeholder='First name'
           type='text'
           name='first_name'
-          // value={}
+          value={this.state.first_name}
+          onChange={this.handleChange}
         />
         <br/>
         <Input
           placeholder='Last name'
           type='text'
           name='last_name'
-        // value={}
+          value={this.state.last_name}
+          onChange={this.handleChange}
         />
         <br />
         <Input
           placeholder='Email'
           type='text'
           name='email'
-        // value={}
+          value={this.state.email}
+          onChange={this.handleChange}
         />
         <br />
         <Input
           placeholder='Password'
           type='text'
           name='password'
-        // value={}
+          value={this.state.password}
+          onChange={this.handleChange}
         />
+        <br />
+        <Button
+          type="submit"
+          onSubmit={this.handleSubmit}
+          >Create account</Button>
         </Form>
+        <br />
+        <a>Login</a>
       </div>
     )
   }
