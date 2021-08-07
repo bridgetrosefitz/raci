5e7540f970db5f1103f8b5ec5f1fd19eb167a345
bridgetrosefitz <bridgetrosefitz@gmail.com>
2021-08-07 16:08:36 -0400

    Removing fat and customizing the landing page

--- src/Components/LandingPage.js
+++ src/Components/LandingPage.js
@@ -34,7 +34,7 @@ const HomepageHeading = ({ mobile }) => (
         fontSize: mobile ? '2em' : '4em',
         fontWeight: 'bold',
         marginBottom: 0,
-        marginTop: mobile ? '0.5em' : '2em',
+        marginTop: mobile ? '1em' : '2em',
         color: '#2185d0'
       }}
     />
@@ -61,9 +61,9 @@ const HomepageHeading = ({ mobile }) => (
     /> */}
     <Button 
       style={{
-        marginTop: mobile ? '1em' : '2.2em'
+        marginTop: mobile ? '2em' : '2.2em'
       }}
-      primary size='huge'>
+      primary size={mobile ? 'medium' : 'huge'}>
       Get Started
       <Icon name='right arrow' />
     </Button>
@@ -224,18 +224,16 @@ export default class LandingPage extends React.Component  {
             <Grid.Row>
               <Grid.Column width={8}>
                 <Header as='h3' style={{ fontSize: '2em' }}>
-                  We Help Companies and Companions
+                  The RACI Framework
                 </Header>
                 <p style={{ fontSize: '1.33em' }}>
-                  We can give your company superpowers to do things that they never thought possible.
-                  Let us delight your customers and empower your needs... through pure data analytics.
+                  Use the Responsible-Accountable-Consulted-Informed framework to make team roles extremely clear.
                 </p>
                 <Header as='h3' style={{ fontSize: '2em' }}>
-                  We Make Bananas That Can Dance
+                  How it works
                 </Header>
                 <p style={{ fontSize: '1.33em' }}>
-                  Yes that's right, you thought it was the stuff of dreams, but even bananas can be
-                  bioengineered.
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                 </p>
               </Grid.Column>
               <Grid.Column floated='right' width={6}>
@@ -244,71 +242,11 @@ export default class LandingPage extends React.Component  {
             </Grid.Row>
             <Grid.Row>
               <Grid.Column textAlign='center'>
-                <Button size='huge'>Check Them Out</Button>
+                <Button size='huge'>Get started</Button>
               </Grid.Column>
             </Grid.Row>
           </Grid>
         </Segment>
-
-        <Segment style={{ padding: '0em' }} vertical>
-          <Grid celled='internally' columns='equal' stackable>
-            <Grid.Row textAlign='center'>
-              <Grid.Column style={{ paddingBottom: '5em', paddingTop: '5em' }}>
-                <Header as='h3' style={{ fontSize: '2em' }}>
-                  "What a Company"
-                </Header>
-                <p style={{ fontSize: '1.33em' }}>That is what they all say about us</p>
-              </Grid.Column>
-              <Grid.Column style={{ paddingBottom: '5em', paddingTop: '5em' }}>
-                <Header as='h3' style={{ fontSize: '2em' }}>
-                  "I shouldn't have gone with their competitor."
-                </Header>
-                <p style={{ fontSize: '1.33em' }}>
-                  <Image avatar src='/images/avatar/large/nan.jpg' />
-                  <b>Nan</b> Chief Fun Officer Acme Toys
-                </p>
-              </Grid.Column>
-            </Grid.Row>
-          </Grid>
-        </Segment>
-
-        <Segment style={{ padding: '8em 0em' }} vertical>
-          <Container text>
-            <Header as='h3' style={{ fontSize: '2em' }}>
-              Breaking The Grid, Grabs Your Attention
-            </Header>
-            <p style={{ fontSize: '1.33em' }}>
-              Instead of focusing on content creation and hard work, we have learned how to master the
-              art of doing nothing by providing massive amounts of whitespace and generic content that
-              can seem massive, monolithic and worth your attention.
-            </p>
-            <Button as='a' size='large'>
-              Read More
-            </Button>
-
-            <Divider
-              as='h4'
-              className='header'
-              horizontal
-              style={{ margin: '3em 0em', textTransform: 'uppercase' }}
-            >
-              <a href='#'>Case Studies</a>
-            </Divider>
-
-            <Header as='h3' style={{ fontSize: '2em' }}>
-              Did We Tell You About Our Bananas?
-            </Header>
-            <p style={{ fontSize: '1.33em' }}>
-              Yes I know you probably disregarded the earlier boasts as non-sequitur filler content, but
-              it's really true. It took years of gene splicing and combinatory DNA research, but our
-              bananas can really dance.
-            </p>
-            <Button as='a' size='large'>
-              I'm Still Quite Interested
-            </Button>
-          </Container>
-        </Segment>
-
         <Segment inverted vertical style={{ padding: '5em 0em' }}>
           <Container>
             <Grid divided inverted stackable>
